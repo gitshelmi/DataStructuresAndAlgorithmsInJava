b5b2826cda636071849316a4a85928a58dacf8dd
Shahab <shahab.helmi@terumobct.com>
2021-04-07 15:51:55 -0600

    fixed formatting and deleted unused lines

--- src/Tests/TwoSumTests.java
+++ src/Tests/TwoSumTests.java
@@ -1,7 +1,6 @@
 package Tests;
 
 import com.problems.TwoSum;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
--- src/com/problems/BaseProblem.java
+++ src/com/problems/BaseProblem.java
@@ -8,13 +8,4 @@ public class BaseProblem {
     public String source;
     public Level level;
     public List<String> tags;
-
-//    public com.company.BaseProblem(String name, String description, String source, Level level, List<String> tags )
-//    {
-//        this.name = name;
-//        this.description = description;
-//        this.source = source;
-//        this.level = level;
-//        this.tags = tags;
-//    }
 }
